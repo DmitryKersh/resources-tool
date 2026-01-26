87d324573ecfdcb446231e062d84335a1608fbfd
dmitrykersh <d.karpukhin@mail.ru>
2026-01-26 11:24:11 +0300

    config self-creation + standalone exe

--- resources-tool.py
+++ resources-tool.py
@@ -1,28 +1,30 @@
 from factories import *
 import json
 
-FACTORY_BRICKS = Factory('bricks', FACTORY_BRICK_CONTRACT, 359)
-FACTORY_CONCRETE = Factory('concrete', FACTORY_CONCRETE_CONTRACT, 258, 1.7)
-FACTORY_FUEL = Factory('fuel', FACTORY_FUEL_CONTRACT, 69, 1.5)
-FACTORY_COPPER = Factory('copper', FACTORY_COPPER_CONTRACT, 58, 1.5)
-FACTORY_ALUMINUM = Factory('aluminum', FACTORY_ALUMINUM_CONTRACT, 50, 1.5)
-FACTORY_MEDTECH = Factory('medtech', FACTORY_MEDTECH_CONTRACT, 39, 1.7)
-FACTORY_STEEL = Factory('steel', FACTORY_STEEL_CONTRACT, 57)
-FACTORY_GLASS = Factory('glass', FACTORY_GLASS_CONTRACT, 48)
-FACTORY_INSECTICIDE = Factory('insecticide', FACTORY_INSECTICIDE_CONTRACT, 25)
-FACTORY_PLASTIC = Factory('plastic', FACTORY_PLASTIC_CONTRACT, 35)
-FACTORY_LITHIUM = Factory('lithium', FACTORY_LITHIUM_CONTRACT, 27)
-FACTORY_ACCUMULATOR = Factory('accumulator', FACTORY_ACCUMULATOR_CONTRACT, 16)
-FACTORY_WEAPONS = Factory('weapons', FACTORY_WEAPONS_CONTRACT, 19)
-FACTORY_SILICON = Factory('silicon', FACTORY_SILICON_CONTRACT, 11)
-FACTORY_CIRCUITRY = Factory('circuitry', FACTORY_CIRCUITRY_CONTRACT, 15)
-FACTORY_TITANIUM = Factory('titanium', FACTORY_TITANIUM_CONTRACT, 17)
-FACTORY_SILVER = Factory('silver', FACTORY_SILVER_CONTRACT, 8)
-FACTORY_GOLD = Factory('gold', FACTORY_GOLD_CONTRACT, 10)
-FACTORY_FERTILIZER = Factory('fertilizer', FACTORY_FERTILIZER_CONTRACT, 39)
-FACTORY_TRUCKS = Factory('trucks', FACTORY_TRUCKS_CONTRACT, 3)
-FACTORY_DRONE = Factory('drone', FACTORY_DRONE_CONTRACT, 9)
-FACTORY_JEWELLERY = Factory('jewellery', FACTORY_JEWELLERY_CONTRACT, 22)
+CONFIG_PATH = 'config.json'
+
+FACTORY_BRICKS = Factory('bricks', FACTORY_BRICK_CONTRACT, 0)
+FACTORY_CONCRETE = Factory('concrete', FACTORY_CONCRETE_CONTRACT, 0)
+FACTORY_FUEL = Factory('fuel', FACTORY_FUEL_CONTRACT, 0)
+FACTORY_COPPER = Factory('copper', FACTORY_COPPER_CONTRACT, 0)
+FACTORY_ALUMINUM = Factory('aluminum', FACTORY_ALUMINUM_CONTRACT, 0)
+FACTORY_MEDTECH = Factory('medtech', FACTORY_MEDTECH_CONTRACT, 0)
+FACTORY_STEEL = Factory('steel', FACTORY_STEEL_CONTRACT, 0)
+FACTORY_GLASS = Factory('glass', FACTORY_GLASS_CONTRACT, 0)
+FACTORY_INSECTICIDE = Factory('insecticide', FACTORY_INSECTICIDE_CONTRACT, 0)
+FACTORY_PLASTIC = Factory('plastic', FACTORY_PLASTIC_CONTRACT, 0)
+FACTORY_LITHIUM = Factory('lithium', FACTORY_LITHIUM_CONTRACT, 0)
+FACTORY_ACCUMULATOR = Factory('accumulator', FACTORY_ACCUMULATOR_CONTRACT, 0)
+FACTORY_WEAPONS = Factory('weapons', FACTORY_WEAPONS_CONTRACT, 0)
+FACTORY_SILICON = Factory('silicon', FACTORY_SILICON_CONTRACT, 0)
+FACTORY_CIRCUITRY = Factory('circuitry', FACTORY_CIRCUITRY_CONTRACT, 0)
+FACTORY_TITANIUM = Factory('titanium', FACTORY_TITANIUM_CONTRACT, 0)
+FACTORY_SILVER = Factory('silver', FACTORY_SILVER_CONTRACT, 0)
+FACTORY_GOLD = Factory('gold', FACTORY_GOLD_CONTRACT, 0)
+FACTORY_FERTILIZER = Factory('fertilizer', FACTORY_FERTILIZER_CONTRACT, 0)
+FACTORY_TRUCKS = Factory('trucks', FACTORY_TRUCKS_CONTRACT, 0)
+FACTORY_DRONE = Factory('drone', FACTORY_DRONE_CONTRACT, 0)
+FACTORY_JEWELLERY = Factory('jewellery', FACTORY_JEWELLERY_CONTRACT, 0)
 
 FACTORIES = [
     FACTORY_BRICKS,
@@ -53,7 +55,7 @@ hint = '''
 2 = Лучшее соотношение с нуля за N апов
 '''
 try:
-    config = json.load(open('config.json'))
+    config = json.load(open(CONFIG_PATH))
     factory_config = config['factories']
     for factory in FACTORIES:
         if factory_config.get(factory.name) is not None:
@@ -64,7 +66,11 @@ try:
         if res_config.get(resource.name) is not None:
             resource.price = res_config[resource.name]
 except Exception as e:
-    print("error reading config")
+    default_fac = {f.name:{'lvl':0, 'speed_mult':1.0} for f in FACTORIES}
+    default_res = {r.name:r.price for r in RESOURCES}
+    with open(CONFIG_PATH, 'w') as configfile:
+        json.dump({'factories':default_fac, 'resources':default_res}, configfile, indent=4)
+    print("Не найден конфиг, поэтому был пересоздан. Перезапусти программу")
 
 
 match input(hint):
@@ -75,4 +81,6 @@ match input(hint):
         for s in bestConfig(FACTORIES, int(input("Сколько апов?"))):
             print(s)
     case _:
-        print("неизв. команда")
+        print("Неизв. команда")
+
+input()
